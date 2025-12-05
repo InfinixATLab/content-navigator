00ca4e8fc1d378b3c3fdc207dc264d8a745dd4c0
Felipe Raulino <felipe.raaulino29@gmail.com>
2025-12-05 10:52:51 -0300

    refactor: adjust project structure for new screens

---
+++ content-navigator/src/app/(home)/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from "expo-router";
+import React from "react";
+
+const StackLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="user-details/[id]" options={{ headerShown: false }} />
+    </Stack>
+  );
+};
+
+export default StackLayout;
--- content-navigator/src/screens/Home/index.tsx
+++ content-navigator/src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { FlatList, ScrollView, Text } from "react-native";
+import { ActivityIndicator, FlatList, Text } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import axios from "axios";
@@ -7,15 +7,19 @@ import UserCard from "@/src/components/UserCard";
 
 const Home = () => {
   const [users, setUsers] = React.useState<User[]>([]);
+  const [loading, setLoading] = React.useState(false);
 
   async function getUsers() {
     const url = process.env.EXPO_PUBLIC_API_URL || "";
+    setLoading(true);
     try {
       const response = await axios.get(url + "?results=20");
       const data = (await response.data) as GetUsersResponse;
       setUsers(data.results);
+      setLoading(false);
     } catch (error) {
       console.log(error);
+      setLoading(false);
     }
   }
 
@@ -26,13 +30,15 @@ const Home = () => {
   return (
     <SafeAreaView className="flex-1 items-center bg-[#f0f0f0]">
       <Text className="text-2xl font-bold text-[#1C1c1c] my-8">User List</Text>
-      <ScrollView>
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
         <FlatList
           data={users}
           renderItem={({ item }) => <UserCard info={item} />}
           keyExtractor={(item, index) => item.id.value + index}
         />
-      </ScrollView>
+      )}
     </SafeAreaView>
   );
 };
