9e39fc7ec619251739ea7fb17e5ea865d1732ed1
Felipe Raulino <felipe.raaulino29@gmail.com>
2025-12-05 09:55:24 -0300

    feat: insert nativewind into project

--- content-navigator/src/screens/Home/index.tsx
+++ content-navigator/src/screens/Home/index.tsx
@@ -4,8 +4,8 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
+    <SafeAreaView className="flex-1 bg-black items-center justify-center">
+      <Text className="text-white text-2xl font-bold">Home</Text>
     </SafeAreaView>
   );
 };
